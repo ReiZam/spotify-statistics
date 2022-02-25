22f613cbb1822decfdc24be0651e63e7807b239b
ReiZam <kmazier@student.42.fr>
2022-02-25 16:31:31 +0100

    re-organise links for header, getLinks function created

--- src/layouts/header.layout.js
+++ src/layouts/header.layout.js
@@ -5,46 +5,46 @@ import Navbar from '../components/navbar/navbar.component.js';
 // PROVIDER
 import {useAuth} from '../providers/auth.provider.js';
 
+function getLinks(auth, authorization_content)
+{
+	if (auth.user)
+	{
+		return ([{ title: "TOP TRACKS", link: "/stats/tracks", external: false },
+		{ title: "TOP GENRES", link: "/stats/genres", external: false },
+		{ title: "TOP ARTISTS", link: "/stats/artists", external: false },
+		{ title: "LOGOUT", link: "/", external: false,
+			onClick: () => {
+				auth.logout(() => {})
+			}
+		}]);
+	}
+	return ([{ title: "TOP TRACKS", link: authorization_content.authorize_link, external: true,
+			onClick: () => {
+				window.localStorage.setItem("code_verifier", authorization_content.code_verifier);
+			}
+		},
+		{ title: "TOP GENRES", link: authorization_content.authorize_link, external: true,
+			onClick: () => {
+				window.localStorage.setItem("code_verifier", authorization_content.code_verifier);
+			}
+		},
+		{ title: "TOP ARTISTS", link: authorization_content.authorize_link, external: true,
+			onClick: () => {
+				window.localStorage.setItem("code_verifier", authorization_content.code_verifier);
+			}
+		},
+		{ title: "LOGIN", link: authorization_content.authorize_link, external: true,
+			onClick: () => {
+				window.localStorage.setItem("code_verifier", authorization_content.code_verifier);
+			}
+		}]);
+}
+
 function Header(props)
 {
 	let auth = useAuth();
 	const {authorization_content} = props;
-
-	const links = [
-		{
-			title: "TOP TRACKS",
-			link: "/stats/tracks",
-			external: false
-		},
-		{
-			title: "TOP GENRES",
-			link: "/stats/genres",
-			external: false
-		},
-		{
-			title: "TOP ARTISTS",
-			link: "/stats/artists",
-			external: false
-		},
-		(!auth.user ?
-		{
-			title: "LOGIN",
-			link: authorization_content.authorize_link,
-			external: true,
-			onClick: () => {
-				window.localStorage.setItem("code_verifier", authorization_content.code_verifier);
-			}
-		}
-		:
-		{
-			title: "LOGOUT",
-			link: "/",
-			external: false,
-			onClick: () => {
-				auth.logout(() => {})
-			}
-		})
-	];
+	const links = getLinks(auth, authorization_content);
 
 	return (
 		<div className="header">

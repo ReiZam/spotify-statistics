c0cc901297709ed0814494b97a7af230a84892ab
ReiZam <kmazier@student.42.fr>
2022-03-29 14:52:57 +0200

    Remove shadow of home cards & set some text light

--- src/components/home/homeConnectCard.component.js
+++ src/components/home/homeConnectCard.component.js
@@ -3,12 +3,12 @@ function HomeConnectCard(props)
 	const {authorization_content} = props;
 
 	return (
-		<div className="grid place-items-center shadow mx-2 rounded-3xl bg-white mx-auto py-8 h-56">
+		<div className="grid place-items-center mx-2 rounded-3xl bg-white mx-auto py-8 h-56">
 			<div className="flex flex-row">
 				<p className="text-4xl font-bold text-spotify_text_color antialiased">Spotify</p>
 				<p className="text-lg font-thin mt-2.5 text-spotify_text_color antialiased">statistics</p>
 			</div>
-			<p className="font-medium py-4 text-center text-spotify_text_color">Please sign in with your Spotify account to view your stats</p>
+			<p className="font-medium py-4 font-light text-center text-spotify_text_color">Please sign in with your Spotify account to view your stats</p>
 			<a href={authorization_content.authorize_link}  onClick={() => {
 					window.localStorage.setItem("code_verifier", authorization_content.code_verifier);
 			}}>
--- src/components/home/homeStatusCard.component.js
+++ src/components/home/homeStatusCard.component.js
@@ -22,7 +22,7 @@ function HomeStatusCard()
 	}, [])
 
 	return (
-		<div className="grid place-items-center shadow rounded-3xl bg-white mx-4 py-8 h-56">
+		<div className="grid place-items-center rounded-3xl bg-white mx-4 py-8 h-56">
 			<div className="flex flex-row">
 				<p className="text-4xl font-bold text-spotify_text_color antialiased">Spotify</p>
 				<p className="text-lg font-thin mt-2.5 text-spotify_text_color antialiased">statistics</p>
@@ -30,7 +30,7 @@ function HomeStatusCard()
 			{!profile ? <Spinner/> :
 			<div className="text-center">
 				<p className="text-xl font-medium">Hello {profile.display_name},</p>
-				<p className="text-sm font-regular">What would you like to know about your musical tastes ?</p>
+				<p className="text-sm font-light">What would you like to know about your musical tastes ?</p>
 			</div>
 			}
 		</div>
